4cbe5dbf40f6cc3a2d61de81df9b46dec17394d1
LaiKe100p <lk_forums@163.com>
2017-10-20 12:25:13 +0800

    Amend end_of_month funcs

--- tsutils.py
+++ tsutils.py
@@ -2,13 +2,26 @@ import datetime as dt
 
 def end_of_month(regDate, months):
 
-    addYear = months // 12
-    addMonth = months - addYear * 12
+    newMonth = regDate.month + months
+    addYear = newMonth // 12
+    newMonth = newMonth % 12
 
-    newMonth = regDate.month + addMonth
     if newMonth == 12:
         res = dt.datetime(regDate.year + addYear, newMonth, 31)
     else:
         res = dt.datetime(regDate.year + addYear, newMonth + 1, 1) - dt.timedelta(1)
-
+    
     return res
+
+
+if __name__ == '__main__':
+
+    A = dt.datetime(2011,12,31)
+    B = dt.datetime(2008,1,3)
+    C = dt.datetime(2007,2,28)
+    D = dt.datetime(2008,2,1)
+
+    print (end_of_month(A, 1))
+    print (end_of_month(B, 1))
+    print (end_of_month(C, 12))
+    print (end_of_month(D, 0))
