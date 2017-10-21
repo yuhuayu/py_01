8dd0bbcaf1104479f2335cc24018fdb93efac4d1
yuhuayu <32922490+yuhuayu@users.noreply.github.com>
2017-10-21 16:29:26 +0800

    Update py_02.py

--- py_02.py
+++ py_02.py
@@ -7,7 +7,6 @@ Created on Fri Oct 20 10:30:34 2017
 
 import pandas as pd
 import numpy as np
-import datetime as dt
 from tsutils import *
 
 df_bond_origin = pd.read_excel("bond.xlsx").set_index('date')
@@ -15,13 +14,19 @@ df_bond = df_bond_origin.dropna()
 df = pd.read_excel("macrofactor.xlsx",sheetname=0).set_index('date')
 df1=df['IVA_yoy'].dropna()
 
-i=0
+i = 0
+count = 0
 while(i < len(df1)-2):
     if df1[i]>df1[i+1] and df1[i+1]>df1[i+2] and df1[i+2]<df1[i+3]:
             print(df1.index[i+3])
-            if df1.index[i+3] in df_bond.index:
-                print(df1.index[i+3],df_bond.loc[df1.index[i+3],'bondreturn'])
+            count = count + 1
             Date_A = df1.index[i+3]
             Date_B = end_of_month(df1.index[i+3],1)
-            print(df_bond.loc[(df_bond.index >= Date_A) & (df_bond.index <= Date_B)])
+            df_period = df_bond.loc[(df_bond.index > Date_A) & (df_bond.index <= Date_B)]
+            mean = df_period['bondreturn'].mean()
+            std = df_period['bondreturn'].std()
+            print('mean:',mean)
+            print('std:',std)
+            print('mean/std:',mean/std,'\n')
     i=i+1
+print(count)
